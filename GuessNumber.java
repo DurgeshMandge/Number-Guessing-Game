42efdb758f487341d9e453216017984dbbc7a91a
Durgesh Mandge <117802796+DurgeshMandge@users.noreply.github.com>
2023-04-09 13:39:14 +0530

    Added Points Score and Guess Display Function

--- GuessNumber.java
+++ GuessNumber.java
@@ -2,8 +2,14 @@ import java.util.*;
 
 class GuessNumber{
     public static void main(String[] args) {
+
+        // Initial variables, Messages and input array
+
         Scanner sc = new Scanner(System.in);
         int score = 0 ;
+        int points = 0;
+        int add = 0;
+        int press = 1;
 
         System.out.println("    _______________--  Guess The Number.  --_______________");
         System.out.println("        You have maximum 5 chances to Guess the number");
@@ -11,19 +17,21 @@ class GuessNumber{
         System.out.println("        The generated number will be between the range of 1 to 100 ");
         System.out.println();
 
-        // Generate the random number using Math.random() method and initialise the array of inputs
 
         int[] inputs = new int[5] ;
 
-        int press = 1;
+        // Game is on while press is equal to 1
 
         while(press==1){
+            
+        // Generate the random number using Math.random() method and initialise the array of inputs
 
             int generatedNumber = (int)(Math.random()*100)+1 ;
-            System.out.println(generatedNumber);
 
             System.out.println("    Enter Your Number. ");
     
+            // Limiting number of attempts to 5
+
             for (int i =1 ; i<= 5 ; i++)
             {
                 
@@ -33,14 +41,19 @@ class GuessNumber{
 
                 if( i!= 5) 
                 {
+                    // For correct Guess
+
                     if ( inputs[i-1] == generatedNumber )
                     {
                         System.out.println("    WOW Super ! Your Guess is Correct ");
                         score+=1;
+                        add = (i==1 || i==2 || i==3)? 5 : 3 ;
+                        points+=add;
                         break;
                     }
     
-                
+                    // For incorrect guess
+                    
                     else
                     {
                         if ( inputs[i-1] > generatedNumber ){
@@ -59,10 +72,14 @@ class GuessNumber{
 
                 else
                 {
+                    // For correct Guess
+
                     if ( inputs[i-1] == generatedNumber )
                     {
                         System.out.println("    Ohhh Great... Your Guess is Correct ");
                         score+=1;
+                        add = (i==1 || i==2 || i==3)? 5 : 3 ;
+                        points+=add;
                         break;
                     }
     
@@ -78,18 +95,26 @@ class GuessNumber{
                         }
     
                         System.out.println("    ans you Have attempted all 5 available attempts:");System.out.println();
-                        System.out.println("    --------The Generated number was " + generatedNumber + " and Your Guesses are :--------");
+                        System.out.println("    --------The Generated number was << " + generatedNumber + " >> and Your Guesses are :--------");
                         System.out.println("                " + Arrays.toString(inputs));System.out.println();
 
                     }
                 }
             }
+
+            //Round completed
+
             System.out.println();
             System.out.println("    Your Score is : " + score);
+            System.out.println("    Your Points are : " + points);
             System.out.println("    Press 1 to Retry, Press any number to Exit");
             press = sc.nextInt();
         }
+
+        // Game Quited
+
         System.out.println("    Number Guessing Game Exited.");
         System.out.println("    Thank You");
+        sc.close();
     }
 }
