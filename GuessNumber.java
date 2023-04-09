bc36f93443ab8b2e791830c1bab450f05f9a00f7
Durgesh Mandge <117802796+DurgeshMandge@users.noreply.github.com>
2023-04-09 11:54:43 +0530

    Update GuessNumber.java

--- GuessNumber.java
+++ GuessNumber.java
@@ -4,46 +4,79 @@ class GuessNumber{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("_______________--  Guess The Number.  --_______________");
-        System.out.println("You have Max 5 chances to Guess the no ... ");
-        System.out.println(" The Number is Between the range of 1 to 100 ");
+        System.out.println("    _______________--  Guess The Number.  --_______________");
+        System.out.println("        You have maximum 5 chances to Guess the number");
+        System.out.println("        If you entere the generated number you will win .");
+        System.out.println("        The generated number will be between the range of 1 to 100 ");
+        System.out.println();
 
-        System.out.println("");
+        // Generate the random number using Math.random() method and initialise the array of inputs
 
-        // int no = sc.nextInt();
-        // System.out.println("Guess The Number. :-  " + no);
-        int min = 0 ;
-        int max = 100 ;
-        Random r = new Random();
-        int x = r.nextInt();
-        // System.out.println(x);
+        int generatedNumber = (int)(Math.random()*100)+1 ;
+        System.out.println(generatedNumber);
+        int[] inputs = new int[5] ;
 
         System.out.println("Enter Your Number. ");
 
         for (int i =1 ; i<= 5 ; i++)
         {
             
-            int no = sc.nextInt();
+            inputs[i-1] = sc.nextInt();
 
-            if (no == x )
+            if( i!= 5) 
             {
-                System.out.println("  Ohhh Great... Your Guess is Correct ");
-                System.exit(0);
-            }
+                if ( inputs[i-1] == generatedNumber )
+                {
+                    System.out.println("  WOW Super ! Your Guess is Correct ");
+                    System.exit(0);
+                }
 
             
-            else
+                else
+                {
+                    if ( inputs[i-1] > generatedNumber ){
+                        System.out.println("Your Guess is Grater than the Number ... ");
+                        System.out.println("Try to guess smaller number than " + inputs[i-1] );
+                    }
+
+                    else {
+                        System.out.println("Your Guess is Smaller than the Number ... ");
+                        System.out.println("Try to guess greater number than " + inputs[i-1]);
+                    }
+                }
+            }else 
             {
-                if (x > no ){
-                    System.out.println("Your Guess is Small than the Number ... ");
-                    System.out.println("Please Try Again  ... ");
+                if ( inputs[i-1] == generatedNumber )
+                {
+                    System.out.println("  Ohhh Great... Your Guess is Correct ");
+                    System.exit(0);
                 }
 
-                else if (x < no){
-                    System.out.println("Your Guess is Greater than the Number ... ");
-                    System.out.println("Please Try Again  ... ");
+            
+                else
+                {
+                    if ( inputs[i-1] > generatedNumber ){
+                        System.out.println("Your Guess is Grater than the Number ... ");
+                    }
+
+                    else {
+                        System.out.println("Your Guess is Smaller than the Number ... ");
+                    }
+
+                    System.out.println("You Have attempted all 5 available attempts:");
+                    System.out.println("Press 1 to retry, Press 2 to exit");
+                    int press = sc.nextInt();
+                    
+                    if( press == 1 ){
+                        //retry
+                    }else if( press == 2 ){
+                        //exit
+                    }else{
+                        System.out.println("Enter either 1 or 2");
+                    }
                 }
             }
+            System.out.println();
         }
 
 
